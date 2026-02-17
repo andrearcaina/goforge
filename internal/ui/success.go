d71762584409786bb8d8eec0cd29811c37172b7c
Andre Arcaina <dtandre331@gmail.com>
2026-02-17 10:52:02 -0500

    refactor UI success logic

--- internal/ui/success.go
+++ internal/ui/success.go
@@ -23,60 +23,80 @@ func OutputSuccess(cfg *spec.Config) {
 	fmt.Println(successStyle.Render("🔥 Generated successfully!"))
 	fmt.Println("Make sure to run the following commands:")
 
-	// base commands to run
+	commands := buildCommands(cfg)
+	printCommands(commands)
+
+	os.Exit(0)
+}
+
+func buildCommands(cfg *spec.Config) []string {
+	makefile := cfg.Form.MakefileFlag
+	db := cfg.Form.DatabaseFlag
+	docker := cfg.Form.DockerFlag
+	isREST := cfg.Form.ServerTypeFlag == spec.REST
+
 	commands := []string{
 		fmt.Sprintf("cd %s", cfg.OutputPath),
 	}
 
-	if cfg.Form.DockerFlag {
-		if cfg.Form.DatabaseFlag {
-			commands = append(commands, "make docker-run  # make sure to have docker installed and running")
-		} else {
-			commands = append(commands, "the docker-compose file only contains a db service, so you can ignore it since you didn't select the database option")
-		}
-	}
-
-	if cfg.Form.MakefileFlag {
-		if cfg.Form.DatabaseFlag {
-			commands = append(commands, "make migrate-up  # make sure you have goose installed and your DB is running")
-		}
-
-		commands = append(commands, "make run")
-
-		printCommands(cfg.Form.ServerTypeFlag, commands)
-	}
-
-	commands = append(commands, "go mod tidy")
-
-	// add commands based on selected options
-	if cfg.Form.ServerTypeFlag == spec.REST {
-		commands = append(commands, "swag init -g ./cmd/server/main.go  # install swag if you haven't already")
-	}
-
-	// if database is selected, add sqlc generate and server run commands
-	if cfg.Form.DatabaseFlag {
+	// docker and db commands if both are set
+	if docker && db {
 		commands = append(commands,
-			"sqlc generate ./...                # install sqlc if you haven't already",
-			"goose up -dir ./db/migrations postgres \"your_db_connection_string\"  # install goose and DB is running",
+			"make docker-run  # make sure to have docker installed and running",
 		)
 	}
 
-	commands = append(commands, "go run ./cmd/server/main.go")
+	// otherwise if only docker is set, then just print a message about the docker-compose file
+	if docker && !db {
+		commands = append(commands,
+			"the docker-compose file only contains a db service, so you can ignore it since you didn't select the database option",
+		)
+	}
 
-	printCommands(cfg.Form.ServerTypeFlag, commands)
-}
+	// makefile and db commands
+	if makefile && db {
+		commands = append(commands,
+			"make migrate-up  # make sure you have goose installed and your DB is running",
+		)
+	}
 
-func printCommands(serverType spec.ServerTypeFlag, commands []string) {
-	if serverType == spec.REST {
+	// regardless if db is set or not, then just run make run if makefile is set
+	if makefile {
+		commands = append(commands, "make run")
+	}
+
+	// non makefile commands if makefile flag not set
+	if !makefile {
+		commands = append(commands, "go mod tidy")
+
+		if db {
+			commands = append(commands,
+				"goose up -dir ./db/migrations postgres \"your_db_connection_string\"  # install goose and DB is running",
+				"sqlc generate ./...                # install sqlc if you haven't already",
+			)
+		}
+
+		if isREST {
+			commands = append(commands,
+				"swag init -g ./cmd/server/main.go  # install swag if you haven't already",
+			)
+		}
+
+		commands = append(commands, "go run ./cmd/server/main.go")
+	}
+
+	// additional command for REST server to view API docs
+	if isREST {
 		commands = append(commands,
 			"visit http://localhost:8000/swagger/index.html  # view API docs",
 		)
 	}
 
+	return commands
+}
+
+func printCommands(commands []string) {
 	for _, cmd := range commands {
 		fmt.Println(cmdStyle.Render(cmd))
 	}
-
-	// exit without error
-	os.Exit(0)
 }
